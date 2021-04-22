84a3585c597729f6b64d83e0725c77c668226c44
bpaquier <bastien.paquier@gmail.com>
2021-04-22 18:57:01 +0200

    finis restaurants page

--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -1,36 +1,116 @@
 import React, { useContext, useEffect, useState } from "react";
-import classnames from "classnames/bind";
+
+import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import { userContext } from "../../context/userContext";
 
-import css from "./styles.module.scss";
 import RestaurantsList from "../../Components/RestaurantsList";
+import Form from "../../Components/Form";
+import Button from "@material-ui/core/Button";
+
+import classnames from "classnames/bind";
+import css from "./styles.module.scss";
 const cx = classnames.bind(css);
 
 export default function RestaurantPage() {
-  const [restaurantsList, setRestaurantsList] = useState(null);
   const { user } = useContext(userContext);
 
-  const getRestaurants = async (id) => {
-    return axios.get(`http://localhost:5000/restaurants/all-restaurants/${id}`);
-  };
+  const [display, setDisplay] = useState("list");
 
+  const [restaurantsList, setRestaurantsList] = useState(null);
   useEffect(() => {
     if (user) {
-      getRestaurants(user?.id).then((rep) =>
-        setRestaurantsList(rep?.data?.data)
-      );
+      getRestaurants(user?.id);
     }
   }, [user]);
 
-  console.log(restaurantsList);
+  const getRestaurants = async (id) => {
+    axios
+      .get(`http://localhost:5000/restaurants/all-restaurants/${id}`)
+      .then((rep) => setRestaurantsList(rep?.data?.data));
+  };
 
+  const handleSubmit = (values) => {
+    const data = { ...values, id_restaurateur: user?.id };
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user?.accessToken}`,
+    };
+    axios
+      .post(`http://localhost:5000/restaurants`, data, {
+        headers: headers,
+      })
+      .then((rep) => {
+        if (rep?.data?.status === 200) {
+          setDisplay("list");
+          getRestaurants(user?.id);
+          toast.success("Restaurant bien enregistré");
+        }
+      })
+      .catch((err) => {
+        const message = err?.response?.data?.errors?.[0];
+        toast.error(message);
+      });
+  };
   return (
     <main>
-      <h1 className={css.title}>Vos restaurants</h1>
-      <section>
-        {restaurantsList && <RestaurantsList list={restaurantsList} />}
-      </section>
+      <Toaster position="bottom-center" />
+      {display === "form" && (
+        <>
+          <div className={css.formContainer}>
+            <div className={css.form}>
+              <Form
+                submitText={"Créer"}
+                title="Ajouter un restaurant"
+                handleSubmit={handleSubmit}
+                inputs={[
+                  {
+                    label: "Nom",
+                    name: "name",
+                    type: "text",
+                    id: 1,
+                  },
+                  {
+                    label: "Adress",
+                    name: "adress",
+                    type: "text",
+                    id: 2,
+                  },
+                  {
+                    label: "Heure d'ouverture",
+                    name: "opening_time",
+                    type: "text",
+                    id: 3,
+                  },
+                ]}
+              />
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setDisplay("list")}
+            >
+              retour à la liste
+            </Button>
+          </div>
+          <div className={css.alert}></div>
+        </>
+      )}
+      {display === "list" && (
+        <>
+          <div className={css.header}>
+            <h1 className={css.title}>Vos restaurants</h1>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setDisplay("form")}
+            >
+              Ajouter un restaurant
+            </Button>
+          </div>
+          {restaurantsList && <RestaurantsList list={restaurantsList} />}
+        </>
+      )}
     </main>
   );
 }
